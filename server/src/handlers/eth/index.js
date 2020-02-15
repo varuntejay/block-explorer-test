e402c05dea40ef7beb692b9fdeeefaf45c52e3a8
Dheeraj Bandela <dheerajkumar.bandela@gmail.com>
2020-02-15 11:59:24 +0530

    PagenationSearch endpoint

--- server/src/handlers/eth/index.js
+++ server/src/handlers/eth/index.js
@@ -5,18 +5,31 @@ const { MONGODB_PROVIDER } = require('../../globalconfig');;
 const router = express.Router();
 let dbConnection;
 getConnection()
-.then((result) => {
-    dbConnection = result;
-})
+    .then((result) => {
+        dbConnection = result;
+    })
 
 
 router.post('/getBlocksWithPagination', async (req, res) => {
-    console.log(dbConnection)
-    const db =  dbConnection.db('eth_db');
-    db.collection('blocks').find({"timestamp": 1}).toArray((err, result) => {
-        console.log(result)
-        res.send({ status: true })
+    const db = dbConnection.db('eth_db');
+    let start = req.body.start || 0
+    let size = req.body.size || 10
+
+    db.collection('blocks').find().skip(start).limit(size).toArray((err, result) => {
+        console.log("blocks", result)
+        res.send(
+            {
+                status: true,
+                result: result
+            })
+
     })
 })
+router.post("/getBlockCount", async (req, res) => {
+    const db = dbConnection.db('eth_db');
+    db.collection('blocks').countDocuments((err, result) => {
+        res.send({ "blockCount": result })
+    })
+});
 
 module.exports = router;
